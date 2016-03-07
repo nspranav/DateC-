34de31f6268e7ff970399e0166d3978237450184
Pranav Ns <PranavNS@pranavs-macbook-air.local>
2016-03-07 13:10:16 -0500

    Initial Commit

---
+++ assign6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  assign6
+//
+//  Created by Pranav Nadigapu on 3/7/16.
+//  Copyright © 2016 Pranav Nadigapu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
